2f18ed913d99c3434e1f7bebd3f5eeed309243db
ErickMarllon <erick.marllon@outlook.com.br>
2023-10-29 03:16:09 -0300

    feat: Remove formatNumber and update discountPrice

--- src/helpers/computeProductTotalPrice.ts
+++ src/helpers/computeProductTotalPrice.ts
@@ -1,37 +1,30 @@
 import { Product } from "@prisma/client";
 
-export interface ProductWithTotalPrice extends Product {
-  totalPrice: string;
-  formattedBasePrice: string;
+export interface ProductWithTotalPrice extends Omit<Product, "basePrice"> {
+  basePrice: number;
+  discountPrice: number;
 }
 
 export const computeProductTotalPrice = (
   product: Product,
 ): ProductWithTotalPrice => {
   const basePrice = Number(product.basePrice);
-  const discountPercentage = Number(product.discountPercentage);
-
-  const formatNumber = (number: number): string => {
-    return number.toLocaleString("pt-BR", {
-      minimumFractionDigits: 2,
-      maximumFractionDigits: 2,
-    });
-  };
+  const discountPercentage = product.discountPercentage / 100;
 
   if (product.discountPercentage === 0) {
     return {
       ...product,
-      totalPrice: formatNumber(basePrice),
-      formattedBasePrice: formatNumber(basePrice),
+      discountPrice: basePrice,
+      basePrice,
     };
   }
 
-  const totalDiscount = basePrice * (discountPercentage / 100);
+  const totalDiscount = basePrice * discountPercentage;
   const discountedPrice = basePrice - totalDiscount;
 
   return {
     ...product,
-    totalPrice: formatNumber(discountedPrice),
-    formattedBasePrice: formatNumber(basePrice),
+    discountPrice: discountedPrice,
+    basePrice,
   };
 };
