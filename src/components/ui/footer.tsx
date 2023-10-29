f25be84fbdbe4a1d0d207d01d5ee866b12b8f488
ErickMarllon <erick.marllon@outlook.com.br>
2023-10-29 05:39:36 -0300

    feat: add footer component

---
+++ src/components/ui/footer.tsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <div className="mt-8 bg-accent px-8 py-4 text-[0.625rem] opacity-75">
+      <p>
+        &copy; Copyright <span className="font-semibold">FSW Store</span>
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
