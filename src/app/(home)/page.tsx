8ee1eb696faab91a3d38748db6042f310805c85f
ErickMarllon <erick.marllon@outlook.com.br>
2023-10-29 03:20:07 -0300

    style: Update styles and image on home page

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -22,10 +22,24 @@ export default async function Home() {
           alt="Até 55% de desconto apenas esse mês!"
           className=" h-auto w-full px-8 pt-8"
           sizes="100vw"
+          quality={100}
           priority
         />
         <Categories />
-        <ProductList products={deals} />
+        <div className="mt-8">
+          <p className="mb-3 pl-8 font-semibold uppercase">Ofertas</p>
+          <ProductList products={deals} />
+        </div>
+        <Image
+          src="/banner-home-02.png"
+          height={0}
+          width={0}
+          alt="Até 55% de desconto em Mouses!"
+          className=" h-auto w-full px-8 pt-8"
+          sizes="100vw"
+          quality={100}
+          priority
+        />
       </div>
     </>
   );
