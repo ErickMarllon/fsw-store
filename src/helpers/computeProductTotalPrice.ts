a456ee3879fae5fd65748f6b08b656f4943d9075
ErickMarllon <erick.marllon@outlook.com.br>
2023-10-28 23:34:58 -0300

    refactor: add formattedBasePrice in computeProductTotalPrice

--- src/helpers/computeProductTotalPrice.ts
+++ src/helpers/computeProductTotalPrice.ts
@@ -1,24 +1,37 @@
 import { Product } from "@prisma/client";
 
 export interface ProductWithTotalPrice extends Product {
-  totalPrice: number;
+  totalPrice: string;
+  formattedBasePrice: string;
 }
 
 export const computeProductTotalPrice = (
   product: Product,
 ): ProductWithTotalPrice => {
+  const basePrice = Number(product.basePrice);
+  const discountPercentage = Number(product.discountPercentage);
+
+  const formatNumber = (number: number): string => {
+    return number.toLocaleString("pt-BR", {
+      minimumFractionDigits: 2,
+      maximumFractionDigits: 2,
+    });
+  };
+
   if (product.discountPercentage === 0) {
     return {
       ...product,
-      totalPrice: Number(product.basePrice),
+      totalPrice: formatNumber(basePrice),
+      formattedBasePrice: formatNumber(basePrice),
     };
   }
 
-  const totalDiscount =
-    Number(product.basePrice) * (product.discountPercentage / 100);
+  const totalDiscount = basePrice * (discountPercentage / 100);
+  const discountedPrice = basePrice - totalDiscount;
 
   return {
     ...product,
-    totalPrice: Number(product.basePrice) - totalDiscount,
+    totalPrice: formatNumber(discountedPrice),
+    formattedBasePrice: formatNumber(basePrice),
   };
 };
